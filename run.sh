c1b7275f69fe66de868916ca67cb36e9dd6e7d5f
opplatek <opplatek@gmail.com>
2022-04-27 16:30:18 -0400

    Automatic Backup @ Wed 27 Apr 16:30:17 EDT 2022

--- run.sh
+++ run.sh
@@ -8,17 +8,17 @@ cd /home/joppelt/playground/amazon_backup/
 ####################################################################################################
 echo "Jan"
 
-echo "ribothrypsis"
-echo "Data local"
-./s3-sync-general.sh configs/s3-sync-data-local.ribothrypsis.config ribothrypsis-data-local
-echo "Analysis"
-./s3-sync-general.sh configs/s3-sync-analysis.ribothrypsis.config
-echo "Samples"
-./s3-sync-general.sh configs/s3-sync-samples.ribothrypsis.config
-echo "Data"
-./s3-sync-general.sh configs/s3-sync-data.ribothrypsis.config
-echo "Main"
-./s3-sync-general.sh configs/s3-sync-main.ribothrypsis.config
+#echo "ribothrypsis" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/s3-sync-data-local.ribothrypsis.config ribothrypsis-data-local
+#echo "Analysis"
+#./s3-sync-general.sh configs/s3-sync-analysis.ribothrypsis.config
+#echo "Samples"
+#./s3-sync-general.sh configs/s3-sync-samples.ribothrypsis.config
+#echo "Data"
+#./s3-sync-general.sh configs/s3-sync-data.ribothrypsis.config
+#echo "Main"
+#./s3-sync-general.sh configs/s3-sync-main.ribothrypsis.config
 
 echo "rna_degradation"
 echo "Data local"
@@ -47,111 +47,111 @@ echo "Main"
 ####################################################################################################
 echo "Manolis"
 
-echo "All directories - deep "
-./s3-sync-general.sh configs/mns/s3-sync-all.deep.config
+#echo "All directories - deep" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#./s3-sync-general.sh configs/mns/s3-sync-all.deep.config
 
-echo "ribothrypsis"
-echo "Data local"
-./s3-sync-general.sh configs/mns/s3-sync-data-local.ribothrypsis.config ribothrypsis-data-local
-echo "Main"
-./s3-sync-general.sh configs/mns/s3-sync-main.ribothrypsis.config
-echo "Analysis"
-./s3-sync-general.sh configs/mns/s3-sync-analysis.ribothrypsis.config
-echo "Data"
-./s3-sync-general.sh configs/mns/s3-sync-data.ribothrypsis.config
-echo "Samples"
-./s3-sync-general.sh configs/mns/s3-sync-samples.ribothrypsis.config
+#echo "ribothrypsis" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/mns/s3-sync-data-local.ribothrypsis.config ribothrypsis-data-local
+#echo "Main"
+#./s3-sync-general.sh configs/mns/s3-sync-main.ribothrypsis.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/mns/s3-sync-analysis.ribothrypsis.config
+#echo "Data"
+#./s3-sync-general.sh configs/mns/s3-sync-data.ribothrypsis.config
+#echo "Samples"
+#./s3-sync-general.sh configs/mns/s3-sync-samples.ribothrypsis.config
 
-echo "ribothrypsis_pirnas"
-echo "Main"
-./s3-sync-general.sh configs/mns/s3-sync-main.ribothrypsis_pirnas.config
-echo "Analysis"
-./s3-sync-general.sh configs/mns/s3-sync-analysis.ribothrypsis_pirnas.config
-echo "Data"
-./s3-sync-general.sh configs/mns/s3-sync-data.ribothrypsis_pirnas.config
-echo "Samples"
-./s3-sync-general.sh configs/mns/s3-sync-samples.ribothrypsis_pirnas.config
+#echo "ribothrypsis_pirnas" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#echo "Main"
+#./s3-sync-general.sh configs/mns/s3-sync-main.ribothrypsis_pirnas.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/mns/s3-sync-analysis.ribothrypsis_pirnas.config
+#echo "Data"
+#./s3-sync-general.sh configs/mns/s3-sync-data.ribothrypsis_pirnas.config
+#echo "Samples"
+#./s3-sync-general.sh configs/mns/s3-sync-samples.ribothrypsis_pirnas.config
 
-echo "fly_kc167"
-echo "Data local"
-./s3-sync-general.sh configs/mns/s3-sync-data-local.fly_kc167.config fly_kc167-data-local
-echo "Main"
-./s3-sync-general.sh configs/mns/s3-sync-main.fly_kc167.config
-echo "Analysis"
-./s3-sync-general.sh configs/mns/s3-sync-analysis.fly_kc167.config
-echo "Data"
-./s3-sync-general.sh configs/mns/s3-sync-data.fly_kc167.config
-echo "Samples"
-./s3-sync-general.sh configs/mns/s3-sync-samples.fly_kc167.config
+#echo "fly_kc167" # Set to archive on 04/27/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/mns/s3-sync-data-local.fly_kc167.config fly_kc167-data-local
+#echo "Main"
+#./s3-sync-general.sh configs/mns/s3-sync-main.fly_kc167.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/mns/s3-sync-analysis.fly_kc167.config
+#echo "Data"
+#./s3-sync-general.sh configs/mns/s3-sync-data.fly_kc167.config
+#echo "Samples"
+#./s3-sync-general.sh configs/mns/s3-sync-samples.fly_kc167.config
 
-echo "fly_RK"
-echo "Data local"
-./s3-sync-general.sh configs/mns/s3-sync-data-local.fly_RK.config fly_RK-data-local
-echo "Main"
-./s3-sync-general.sh configs/mns/s3-sync-main.fly_RK.config
-echo "Analysis"
-./s3-sync-general.sh configs/mns/s3-sync-analysis.fly_RK.config
-echo "Data"
-./s3-sync-general.sh configs/mns/s3-sync-data.fly_RK.config
-echo "Samples"
-./s3-sync-general.sh configs/mns/s3-sync-samples.fly_RK.config
+#echo "fly_RK" # Set to archive on 04/27/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/mns/s3-sync-data-local.fly_RK.config fly_RK-data-local
+#echo "Main"
+#./s3-sync-general.sh configs/mns/s3-sync-main.fly_RK.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/mns/s3-sync-analysis.fly_RK.config
+#echo "Data"
+#./s3-sync-general.sh configs/mns/s3-sync-data.fly_RK.config
+#echo "Samples"
+#./s3-sync-general.sh configs/mns/s3-sync-samples.fly_RK.config
 
-echo "mouse_FUS"
-echo "Data local"
-./s3-sync-general.sh configs/mns/s3-sync-data-local.mouse_FUS.config mouse_FUS-data-local
-echo "Main"
-./s3-sync-general.sh configs/mns/s3-sync-main.mouse_FUS.config
-echo "Analysis"
-./s3-sync-general.sh configs/mns/s3-sync-analysis.mouse_FUS.config
-echo "Data"
-./s3-sync-general.sh configs/mns/s3-sync-data.mouse_FUS.config
-echo "Samples"
-./s3-sync-general.sh configs/mns/s3-sync-samples.mouse_FUS.config
+#echo "mouse_FUS" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/mns/s3-sync-data-local.mouse_FUS.config mouse_FUS-data-local
+#echo "Main"
+#./s3-sync-general.sh configs/mns/s3-sync-main.mouse_FUS.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/mns/s3-sync-analysis.mouse_FUS.config
+#echo "Data"
+#./s3-sync-general.sh configs/mns/s3-sync-data.mouse_FUS.config
+#echo "Samples"
+#./s3-sync-general.sh configs/mns/s3-sync-samples.mouse_FUS.config
 
-echo "human_HRI"
-echo "Data local"
-./s3-sync-general.sh configs/mns/s3-sync-data-local.human_HRI.config human_HRI-data-local
-echo "Main"
-./s3-sync-general.sh configs/mns/s3-sync-main.human_HRI.config
-echo "Analysis"
-./s3-sync-general.sh configs/mns/s3-sync-analysis.human_HRI.config
-echo "Data"
-./s3-sync-general.sh configs/mns/s3-sync-data.human_HRI.config
-echo "Samples"
-./s3-sync-general.sh configs/mns/s3-sync-samples.human_HRI.config
+#echo "human_HRI" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/mns/s3-sync-data-local.human_HRI.config human_HRI-data-local
+#echo "Main"
+#./s3-sync-general.sh configs/mns/s3-sync-main.human_HRI.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/mns/s3-sync-analysis.human_HRI.config
+#echo "Data"
+#./s3-sync-general.sh configs/mns/s3-sync-data.human_HRI.config
+#echo "Samples"
+#./s3-sync-general.sh configs/mns/s3-sync-samples.human_HRI.config
 
-echo "akron"
-echo "Data local"
-./s3-sync-general.sh configs/mns/s3-sync-data-local.akron.config akron-data-local
-echo "Main"
-./s3-sync-general.sh configs/mns/s3-sync-main.akron.config
-echo "Analysis"
-./s3-sync-general.sh configs/mns/s3-sync-analysis.akron.config
-echo "Data"
-./s3-sync-general.sh configs/mns/s3-sync-data.akron.config
-echo "Samples"
-./s3-sync-general.sh configs/mns/s3-sync-samples.akron.config
+#echo "akron" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/mns/s3-sync-data-local.akron.config akron-data-local
+#echo "Main"
+#./s3-sync-general.sh configs/mns/s3-sync-main.akron.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/mns/s3-sync-analysis.akron.config
+#echo "Data"
+#./s3-sync-general.sh configs/mns/s3-sync-data.akron.config
+#echo "Samples"
+#./s3-sync-general.sh configs/mns/s3-sync-samples.akron.config
 
 ####################################################################################################
 echo "Panos"
 
-echo "All directories - Ago, Akron, tools"
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.ago.config
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.akron.config
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.tools.config
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.simulation.config
+#echo "All directories - Ago, Akron, tools" # Set to archive on 02/14/2022 - not updated anymore; not deleted locally yet
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.ago.config
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.akron.config
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.tools.config
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-all.simulation.config
 
-echo " Miwi"
-echo "Data local"
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-data-local.miwi.config miwi-data-local
-echo "Data"
-./s3-sync-eneral.sh configs/pan.alexiou/s3-sync-data.miwi.config
-echo "Main"
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-main.miwi.config
-echo "Analysis"
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-analysis.miwi.config
-echo "Samples"
-./s3-sync-general.sh configs/pan.alexiou/s3-sync-samples.miwi.config
+#echo " Miwi" # Set to archive on 04/27/2022 - not updated anymore; not deleted locally yet
+#echo "Data local"
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-data-local.miwi.config miwi-data-local
+#echo "Data"
+#./s3-sync-eneral.sh configs/pan.alexiou/s3-sync-data.miwi.config
+#echo "Main"
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-main.miwi.config
+#echo "Analysis"
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-analysis.miwi.config
+#echo "Samples"
+#./s3-sync-general.sh configs/pan.alexiou/s3-sync-samples.miwi.config
 
 #echo "DDX3" # Set to archive on 06/02/2021 - not updated anymore & deleted locally
 #echo "Data local"
